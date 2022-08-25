fd50e8ca901870c7898613475ac99b6173c22790
MD  Zaid <90028171+mdzaid8920@users.noreply.github.com>
2022-08-25 12:59:55 +0500

    First Commit in which  I can send Email using Nodejs

---
+++ index.js
@@ -0,0 +1,26 @@
+const nodeCron = require("node-cron");
+const nodeMailer = require("nodemailer");
+
+// function to send an mail using Gmail service
+
+let emailTransporter = nodeMailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "youremail@gmail.com",
+    pass: "", // your gmail or App Password password
+  },
+});
+
+let options = {
+  from: "muhammadzaidabcd@gmail.com",
+  to: "45224@gcslahore.edu.pk",
+  subject: "Test Send Email using JavaScript",
+  text: "Hello this is test email by MD ZAID...",
+};
+
+emailTransporter.sendMail(options, (err, info) => {
+  if (err) console.log(err);
+  else {
+    console.log("Email Sent successfully using nodejs");
+  }
+});
